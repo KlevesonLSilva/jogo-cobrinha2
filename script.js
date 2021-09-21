5d9aa05f82c2b2a7a8420f94cc615a9a274e4d72
newba <kleveson_x3@hotmail.com>
2021-09-21 19:50:48 -0300

    definir direções e impedir que cobinha suma

--- script.js
+++ script.js
@@ -20,7 +20,22 @@ function criarCobrinha(){
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction ="left"
+    if(event.keyCode == 38 && direction != "donw") direction = "up"
+    if(event.keyCode == 39 && direction != "left") direction = "right"
+    if(event.keyCode == 40 && direction != "up") direction = "down"
+}
+
 function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "right") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "left") snake[0].y = 16 * box;
+
+
     criarBG();
     criarCobrinha();
 
@@ -42,4 +57,5 @@ function iniciarJogo(){
     snake.unshift(newHead);
 }
 
-let jogo = setInverval(iniciarJogo, 100);
+
+let jogo = setInterval(iniciarJogo, 100);
